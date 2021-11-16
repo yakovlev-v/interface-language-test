35dfce0afa0f83696f1ccf4d4f6adf59bce033a5
yakovlev-v <61628407+yakovlev-v@users.noreply.github.com>
2021-11-16 22:44:14 +0500

    Изменил название тестовой функции

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_interface_language(browser):
+def test_button_exist(browser):
     browser.get(url)
     assert len(
         browser.find_elements_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")) == 1, "А где кнопка?!"
