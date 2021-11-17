a5e21df32f9303fa3ade67e49a220cfb0565ceac
yakovlev-v <61628407+yakovlev-v@users.noreply.github.com>
2021-11-17 21:57:10 +0500

    Поправил сообщение в assert на более подходящее

--- test_items.py
+++ test_items.py
@@ -3,5 +3,5 @@ url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_button_exist(browser):
     browser.get(url)
-    assert len(
-        browser.find_elements_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")) == 1, "А где кнопка?!"
+    a = len(browser.find_elements_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket"))
+    assert a == 1, 'На странице нет кнопки "Добавить в корзину"'
